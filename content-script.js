40f100f0f03f7a1247e272e80fa5c25b3678753e
Miki Ashkenazi <miki.ashkenazi@hibob.io>
2022-04-29 17:13:46 +0300

    options page

--- content-script.js
+++ content-script.js
@@ -1,57 +1,66 @@
-const loginAppSelector = 'app-login';
-const emailSelector = 'input[type=email]';
-const pwrdSelector = 'input[type=password]';
-const nextButtonSelector = '.email-next-button button';
-const welcomBackSelector = 'b-avatar-image.avatar';
+const loginAppSelector = "app-login";
+const emailSelector = "input[type=email]";
+const pwrdSelector = "input[type=password]";
+const nextButtonSelector = ".email-next-button button";
+const welcomBackSelector = "b-avatar-image.avatar";
 const printedEmailSelector =
-  welcomBackSelector + ' + div, b-back-button ~ b-display-1 + div';
-const oktaButtonSelector = '.inner-wrapper b-button[sso-method=okta-btn] button';
+  welcomBackSelector + " + div, b-back-button ~ b-display-1 + div";
+const oktaButtonSelector =
+  ".inner-wrapper b-button[sso-method=okta-btn] button";
 const switchAccountLinkSelector =
-  '.password-wrapper + div > b-text-button + div a, .inner-wrapper b-button[sso-method=okta-btn] + div > div > a';
+  ".password-wrapper + div > b-text-button + div a, .inner-wrapper b-button[sso-method=okta-btn] + div > div > a";
 const pageLoadedSelector = `input, ${printedEmailSelector}`; // used to determine if the vital elements are loaded
 const steps = {
-  email: 'email',
-  pwrd: 'password',
-  okta: 'okta',
-  unknown: 'unknown',
+  email: "email",
+  pwrd: "password",
+  okta: "okta",
+  unknown: "unknown",
 };
-const logins = [
-  {
-    label: 'Demo',
-    email: 'miki.ashkenazi+demo@hibob.io',
-    password: 'SecretDemo1',
-  },
-  {
-    label: 'mi3',
-    email: 'miki.ashkenazi+mi3@hibob.io',
-    password: 'SecretDemo3',
-  },
-  {
-    label: 'bob',
-    email: 'miki.ashkenazi@hibob.io',
-    okta: true,
-  },
-];
+  // let myLogins = [
+  // {
+  //   label: "Demo",
+  //   email: "miki.ashkenazi+demo@hibob.io",
+  //   password: "SecretDemo1",
+  // },
+  // {
+  //   label: "mi3",
+  //   email: "miki.ashkenazi+mi3@hibob.io",
+  //   password: "SecretDemo3",
+  // },
+  // {
+  //   label: "bob",
+  //   email: "miki.ashkenazi@hibob.io",
+  //   okta: true,
+  // },
+  // ];
 let myContent;
 
 (function onload() {
   // console.log('Bogin extenstion', 'onload');
-  // observePageChange();
-  waitUntilElementExists(loginAppSelector, createLayer);
+  chrome.storage.sync.get(null, (store) => {
+    if (!store.data)
+      chrome.storage.sync.set({ data: [] }, () => start(store));
+    else start(store);
+  });
 })();
+
+function start(store) {
+  myLogins = store.data;
+  listenForStorageChanges();
+  waitUntilElementExists(loginAppSelector, createLayer);
+}
 
 function createLayer() {
   const loginElement = document.querySelector(loginAppSelector);
   if (!loginElement) {
     return;
   }
-  myContent = document.createElement('div');
-  myContent.id = 'boginExtContent';
+  myContent = document.createElement("div");
+  myContent.id = "boginExtContent";
   waitUntilElementExists(pageLoadedSelector, createClickableLogins);
   try {
     loginElement.appendChild(myContent);
-  } catch (error) {
-  }
+  } catch (error) {}
 }
 
 function createClickableLogins() {
@@ -61,12 +70,12 @@ function createClickableLogins() {
       : null;
 
   alreadyLoggedInEmail &&
-    sel(switchAccountLinkSelector).addEventListener('click', function () {
-      myContent.innerHTML = '';
+    sel(switchAccountLinkSelector).addEventListener("click", function () {
+      myContent.innerHTML = "";
       waitUntilElementExists(pageLoadedSelector, createClickableLogins);
     });
 
-  for (loginConfig of logins) {
+  for (loginConfig of myLogins) {
     if (whereAmI() === steps.okta) {
       createOktaClickable();
       break;
@@ -78,23 +87,30 @@ function createClickableLogins() {
       continue;
     }
     const login = loginConfig; // to avoid closure
-    const clickable = document.createElement('button');
-    clickable.type = 'button';
-    clickable.className = 'clickable';
+    const clickable = document.createElement("button");
+    const className = loginConfig.okta ? 'clickable okta' : 'clickable';
+    clickable.type = "button";
+    clickable.className = className;
     clickable.innerText = loginConfig.label;
-    clickable.addEventListener('click', function () {
+    clickable.addEventListener("click", function () {
       optionClicked(login);
+    });
+    clickable.addEventListener("mouseover", function () {
+      optionHovered(login);
+    });
+    clickable.addEventListener("mouseout", function () {
+      optionUnhovered();
     });
     myContent.appendChild(clickable);
   }
 }
 
 function createOktaClickable() {
-  const clickable = document.createElement('button');
-  clickable.type = 'button';
-  clickable.className = 'clickable';
-  clickable.innerText = logins.find(login => login.okta).label;
-  clickable.addEventListener('click', function () {
+  const clickable = document.createElement("button");
+  clickable.type = "button";
+  clickable.className = "clickable";
+  clickable.innerText = myLogins.find((login) => login.okta).label;
+  clickable.addEventListener("click", function () {
     sel(oktaButtonSelector).click();
   });
   myContent.appendChild(clickable);
@@ -110,9 +126,21 @@ function optionClicked(loginConfig) {
   }
 }
 
+function optionHovered(loginConfig) {
+  if (whereAmI() === steps.email) {
+    sel(emailSelector).value = loginConfig.email;
+  }
+}
+
+function optionUnhovered() {
+  if (whereAmI() === steps.email) {
+    sel(emailSelector).value = '';
+  }
+}
+
 function fillEmail(loginConfig) {
   sel(emailSelector).value = loginConfig.email;
-  sel(emailSelector).dispatchEvent(new Event('input', { bubbles: true }));
+  sel(emailSelector).dispatchEvent(new Event("input", { bubbles: true }));
   getNextButton().click();
 }
 
@@ -120,7 +148,7 @@ function fillPassword(loginConfig) {
   setTimeout(() => {
     const pwrd = document.querySelector(pwrdSelector);
     pwrd.value = loginConfig.password;
-    pwrd.dispatchEvent(new Event('input', { bubbles: true }));
+    pwrd.dispatchEvent(new Event("input", { bubbles: true }));
     getNextButton().click();
   }, 1000);
 }
@@ -150,7 +178,7 @@ function waitUntilElementExists(selector, callback, repetitions = 12) {
       callback();
     }
     if (++i === repetitions) {
-        window.clearInterval(intervalId);
+      window.clearInterval(intervalId);
     }
   }, 100);
 }
@@ -163,30 +191,23 @@ function sel(selector) {
   return document.querySelector(selector);
 }
 
-// function observePageChange() {
-//   const observedContainer = sel(loginApp);
-//   const callback = function (mutations) {
-//     let email, password, welcomBack;
-//     mutations.forEach(function (mutation) {
-//       if (mutation.addedNodes[0]) {
-//         switch (mutation.addedNodes[0]?.tagName?.toLowerCase()) {
-//           case 'app-login-form-password':
-//             password = true;
-//             break;      
-//           case 'app-login-form-email':
-//             email = true;
-//             break;
-//           case 'app-welcome-back':
-//             welcomBack = true;
-//             break;
-//         }
-//       }
-//     });
-//     const change = email ? 'email' : welcomBack && password ? 'welcomeBackPassword' : welcomBack && !password ? 'welcomeBackOkta' : null;
-//   };
+// Storage
 
-//   const observer = new MutationObserver(callback);
-//   if (observedContainer) {
-//     observer.observe(observedContainer, { subtree: true, childList: true });
-//   }
-// }
+
+function listenForStorageChanges() {
+  chrome.storage.onChanged.addListener((changes, areaName) => {
+    const storeLogins = changes.data.newValue;
+    myContent.innerHTML = ""; // clear existing clickable logins
+    if (Array.isArray(storeLogins) && storeLogins.length) {
+      createStoreLogins(storeLogins);
+    } else {
+      myLogins = [];
+    }
+  });
+}
+
+function createStoreLogins(storeLogins) {
+  myLogins = storeLogins;
+  createClickableLogins();
+}
+
---
+++ options.js
@@ -0,0 +1,118 @@
+let data = [];
+const OKTA_FORM_NAME = 'formOkta';
+
+function loaded() {
+  bindButtonsClick();
+  chrome.storage.sync.get(null, (store) => {
+    if (!store.data) {
+      chrome.storage.sync.set({ data: [] }, () => data = []);
+    } else {
+      data = store.data;
+    }
+    populateForms(data);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", loaded);
+
+function bindButtonsClick() {
+  document.querySelectorAll('[type=button][name=save]').forEach(btn => btn.addEventListener("click", saveClicked));
+  document.querySelectorAll('[type=button][name=delete]').forEach(btn => btn.addEventListener("click", deleteClicked));
+}
+
+function saveClicked() {
+  const formContainer = event.target.form;
+  const okta = formContainer.name === OKTA_FORM_NAME;
+  const labelEl = formContainer.label;
+  const emailEl = formContainer.email;
+  const passwordEl = formContainer?.password;
+
+  const label = labelEl.value;
+  const email = emailEl.value;
+  const password = passwordEl ? passwordEl.value : null;
+
+  if (!label || !email) {
+    response('Label and Email are mandatory');
+    return;
+  }
+
+  labelEl.disabled = true;
+  emailEl.disabled = true;
+  formContainer.save.disabled = true;
+  if (passwordEl) { passwordEl.disabled = true; }
+
+  data = [...data, { label, email, password, okta }];
+  // log(data);
+  saveToStorage(data);
+}
+
+function deleteClicked() {
+  const formContainer = event.target.form;
+  const email = formContainer.email.value;
+
+  clearRow(formContainer);
+  data = data.filter(item => item.email !== email);
+  saveToStorage(data);
+}
+
+function saveToStorage(data) {
+  chrome.storage.sync.set({ data },
+    () => response('Saved'));
+}
+
+function populateForms(storeData) {
+  // log(storeData);
+  // [{"email":"test1@kaka.com","label":"comp1","password":"kaka"}]
+  storeData.forEach((item, index) => {
+    const formName = item.okta ? OKTA_FORM_NAME : `form${index+1}`;
+    const labelEl = document[formName].label;
+    const emailEl = document[formName].email;
+    const passwordEl = document[formName]?.password;
+    if (passwordEl) {
+      passwordEl.value = item.password;
+      passwordEl.disabled = true;
+    }
+    labelEl.value = item.label;
+    emailEl.value = item.email;
+    labelEl.disabled = true;
+    emailEl.disabled = true;
+    document[formName].save.disabled = true;
+  });
+}
+
+function clearRow(frm) {
+  frm.label.value = '';
+  frm.email.value = '';
+  if (frm.password) {
+    frm.password.value = ''; 
+    frm.password.disabled = false; 
+  }
+  frm.label.disabled = false;
+  frm.email.disabled = false;
+  frm.save.disabled = false;
+  response('Row cleared');
+}
+
+sel("#clearAll").addEventListener("click", () => {
+  Array.from(document.forms).forEach(form => clearRow(form));
+  chrome.storage.sync.set({ data: [] }, () => {
+    data = [];
+    response('All data cleared');
+  });
+});
+
+function sel(selector) {
+  return document.querySelector(selector);
+}
+
+function response(text) {
+  sel("#msg").innerText = text;
+  setTimeout(() => {
+    sel("#msg").innerText = '';
+  }, 1800);
+}
+
+function log(thing) {
+  sel('#log').innerText = JSON.stringify(thing);
+}
+
